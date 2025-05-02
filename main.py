6c6ebf48bd6ff0b9e720209564165e7bcb3d4380
tima1404 <t.lihatskiy@gmail.com>
2025-05-02 16:53:32 +0300

    HW 1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello world')
+name = input('Як тебе звати? ')
+age = int(input('Скільки тобі років? '))
+if age >= 18:
+    admission = 'Вхід дозволено!'
+else:
+    admission = 'На жаль, вхід заборонено.'
+print('Привіт,',name,', тобі',age,'років.',admission)
